bc501849ecdc4a81210507b117698d9a046b1574
Nelson <nelsonsanabio5d@gmail.com>
2017-09-30 01:42:40 +0500

    Examen

---
+++ Practica1/Ejercicio2/Ejercicio2.R
@@ -0,0 +1,6 @@
+#Nombre : Nelson Sanabio Maldonado 20152734A
+#El uso de la funcion c() en la cual veremos mas adelante sobre sus usos
+
+vector1<-c(1:100)*c(1,-1)
+vector2<-factorial(1:100)
+vector3<-c(1:100)^2
