aa88f447b4f701e13f6311eb64c2291c9d99f487
Nelson <nelsonsanabio5d@gmail.com>
2017-11-10 16:04:18 +0500

    Practica2

--- Practica2/Ejercicio3.R
+++ Practica2/Ejercicio3.R
@@ -1,3 +1,12 @@
-nth <- function(x,n){
-        x
+# NOMBRE : Nelson Sanabio Maldonado
+# Respuesta 3 :
+#Creando la funcion nth
+nth<- function(x,n){
+        m <- which(x)
+        cat(m[n])
 }
+
+#Ejemplo de la Hoja
+x <- c(1,2,4,2,1,3)
+nth(x > 2,2)
+nth(x > 4,2)
