f4601fc7ce221e7c62bb0b63e23d076808d70be7
Nelson <nelsonsanabio5d@gmail.com>
2017-11-24 16:52:50 +0500

    Practica3

---
+++ Practic3/Ejercicio6/Ejercicio6.R
@@ -0,0 +1,14 @@
+#Nelson Sanabio Maldonado
+
+iris <- factor(c("Setosa","versicolor","Virginica"))
+#Primer punto  
+# El objeto iris es datframe.
+# Tiene 150 lineas
+# Tiene 5 columnas
+
+#Segundo punto 
+#Te gráfica una tabla contabiliza las palabras que existen en el factor, en mi caso solo 
+#aparece 1 de cada uno, para saber cuantas de tales palabras estan en el factor
+
+#Tercer punto  
+#
